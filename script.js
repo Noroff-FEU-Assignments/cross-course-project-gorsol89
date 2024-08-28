1e69444c760b4bfabca61f89d5c3a3b184e0aa63
gorsol89 <gorsunsol@gmail.com>
2024-08-28 12:43:13 +0200

    started with js.script with no luck

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const url= ""
+const url= "http://localhost/flower-power.local/wp-jason/wc/store/products";
+
+async getProducts() {
+    try {
+        const response = await fetch(url);
+        const getResults = await response.json();
+        console.log(getResults);
+    }
+
+    catch(error);
+}
+
+getProducts();
