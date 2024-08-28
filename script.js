e226f0935d123ff46a519ac824437677e809f257
gorsol89 <gorsunsol@gmail.com>
2024-08-28 12:58:02 +0200

    did the innerHTML method

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-const url= "http://localhost/flower-power.local/wp-jason/wc/store/products";
+const url = "http://flowerpower.local/wp-json/wc/store/products";
 
-async getProducts() {
+async function getProducts() {
     try {
         const response = await fetch(url);
-        const getResults = await response.json();
-        console.log(getResults);
+        const products = await response.json();
+        displayProducts(products);
+    } catch (error) {
+        console.error("Problem with getting the products:", error);
     }
+}
 
-    catch(error);
+function displayProducts(products) {
+    const productsContainer = document.querySelector('.products');
+    productsContainer.innerHTML = '';
+
+    products.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.className = 'product-card';
+
+        productCard.innerHTML = `
+            <img src="${product.images[0].src}" alt="${product.name}">
+            <h4>${product.name}</h4>
+            <p>${product.price_html}</p>
+            <button>View Product</button>
+        `;
+
+        productsContainer.appendChild(productCard);
+    });
 }
 
 getProducts();
